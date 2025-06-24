f5142949673390969ceb448411a12a153f912185
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-06-24 22:43:39 +0700

    Membuat ServiceHttpClient sebagai penghubung ke backend

---
+++ lib/services/service_http_client.dart
@@ -0,0 +1,66 @@
+import 'dart:convert';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+
+class ServiceHttpClient {
+  final String baseUrl = 'http://10.0.2.2:8000/api/';
+  final secureStorage = FlutterSecureStorage();
+
+  Future<http.Response> post(String endPoint, Map<String, dynamic> body) async {
+    final url = Uri.parse("$baseUrl$endPoint");
+    try {
+      final response = await http.post(
+        url,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(body),
+        );
+      return response;
+    } catch (e) {
+      throw Exception("POST request failed: $e");
+    }
+  }
+
+  Future<http.Response> postWithToken(
+    String endPoint, 
+    Map<String, dynamic> body,
+    ) async {
+      final token = await secureStorage.read(key: "authToken");
+      final url = Uri.parse("$baseUrl$endPoint");
+      try {
+        final response = await http.post(
+          url,
+          headers: {
+            'Authorization': 'Bearer $token',
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: jsonEncode(body),
+          );
+        return response;
+      } catch (e) {
+        throw Exception("POST request failed: $e");
+    }
+  }
+  
+  Future<http.Response> get(String endPoint) async {
+    final token = await secureStorage.read(key: "authToken");
+    final url = Uri.parse("$baseUrl$endPoint");
+    try {
+      final response = await http.get(
+        url,
+        headers: {
+          'Authorization': 'Bearer $token',
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+      );
+      return response;
+    } catch (e) {
+      throw Exception("GET request failed: $e");
+    }
+  }
+}
