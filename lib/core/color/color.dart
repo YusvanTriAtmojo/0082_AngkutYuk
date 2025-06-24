02fff1c53082992aab8af64d1c09e210c8faf323
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-06-24 23:44:39 +0700

    Membuat class untuk menyimpan warna

---
+++ lib/core/color/color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Warna {
+  static const List<Color> unguGradasi = [
+    Color(0xFF7C3AED),
+    Color(0xFF8B5CF6),
+  ]; 
+  static const Color ungu = Color(0xFF7C3AED);
+  static const Color unguMuda = Color(0xFF8B5CF6); 
+  static const Color orange = Color(0xFFF97316);
+  static const Color ungubackgorund = Color(0xFFEEE9FF);
+}
