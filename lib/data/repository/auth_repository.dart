c40638eed545921b76a172e97aa93db615e4c6b2
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-06-24 23:02:43 +0700

    Menambahkan metode login untuk autentikasi user

--- lib/data/repository/auth_repository.dart
+++ lib/data/repository/auth_repository.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 import 'dart:developer';
 
+import 'package:angkut_yuk/data/model/request/auth/login_request_model.dart';
 import 'package:angkut_yuk/data/model/request/auth/register_request_model.dart';
+import 'package:angkut_yuk/data/model/response/auth_response_model.dart';
 import 'package:angkut_yuk/services/service_http_client.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
@@ -11,6 +13,37 @@ class AuthRepository {
   final secureStorage = FlutterSecureStorage();
 
   AuthRepository(this._serviceHttpClient);
+
+  Future<Either<String, AuthResponseModel>> login(
+    LoginRequestModel requestModel,
+  ) async {
+    try {
+      final response = await _serviceHttpClient.post(
+        "login",
+        requestModel.toMap(),
+      );
+      final jsonResponse = json.decode(response.body);
+      if (response.statusCode == 200) {
+        final loginResponse = AuthResponseModel.fromMap(jsonResponse);
+        await secureStorage.write(
+          key: "authToken",
+          value: loginResponse.user!.token,
+        );
+        await secureStorage.write(
+          key: "userRole",
+          value: loginResponse.user!.role,
+        );
+        log("Login successful: ${loginResponse.message}");
+        return Right(loginResponse);
+      } else {
+        log("Login failed: ${jsonResponse['message']}");
+        return Left(jsonResponse['message'] ?? "Login failed");
+      }
+    } catch (e) {
+      log("Error in login: $e");
+      return Left("An error occurred while logging in.");
+    }
+  }
 
   Future<Either<String, String>> register(
     RegisterRequestModel requestModel,
