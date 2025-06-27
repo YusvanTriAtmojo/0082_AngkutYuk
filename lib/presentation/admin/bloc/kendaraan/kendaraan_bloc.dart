531efbb86793aa271f4746b3bffe2595c88aeb37
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-06-27 15:30:47 +0700

    Menambahkan kendaraan bloc yang mengatur get, create, delete kendaraan

---
+++ lib/presentation/admin/bloc/kendaraan/kendaraan_bloc.dart
@@ -0,0 +1,68 @@
+import 'package:angkut_yuk/data/model/request/admin/kendaraan_request_model.dart';
+import 'package:angkut_yuk/data/model/response/get_all_kendaraan_response_model.dart';
+import 'package:angkut_yuk/data/repository/kendaraan_repository.dart';
+import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:meta/meta.dart';
+
+part 'kendaraan_event.dart';
+part 'kendaraan_state.dart';
+
+class KendaraanBloc extends Bloc<KendaraanEvent, KendaraanState> {
+  final KendaraanRepository kendaraanRepository;
+
+  KendaraanBloc({required this.kendaraanRepository}) : super(KendaraanInitial()) {
+    on<KendaraanRequested>(_onKendaraanRequested);
+    on<KendaraanCreateRequested>(_onKendaraanCreateRequested);
+    on<KendaraanDeleted>(_onKendaraanDeleted);
+  }
+
+  Future<void> _onKendaraanRequested(
+    KendaraanRequested event,
+    Emitter<KendaraanState> emit,
+  ) async {
+    emit(KendaraanLoading());
+
+    final Either<String, GetAllKendaraanModel> result =
+        await kendaraanRepository.getAllKendaraan();
+
+    result.fold(
+      (failure) => emit(KendaraanFailure(error: failure)),
+      (data) => emit(KendaraanLoaded(listKendaraan: data.dataKendaraan)),
+    );
+  }
+
+  Future<void> _onKendaraanCreateRequested(
+    KendaraanCreateRequested event,
+    Emitter<KendaraanState> emit,
+  ) async {
+    emit(KendaraanLoading());
+
+    final result = await kendaraanRepository.createKendaraan(event.requestModel);
+
+    result.fold(
+      (failure) => emit(KendaraanFailure(error: failure)),
+      (message) => emit(KendaraanOperationSuccess(message: message)),
+    );
+  }
+
+  Future<void> _onKendaraanDeleted(
+    KendaraanDeleted event,
+    Emitter<KendaraanState> emit,
+  ) async {
+    emit(KendaraanLoading());
+
+    final result = await kendaraanRepository.deleteKendaraan(event.id);
+
+    await result.fold(
+      (error) async => emit(KendaraanFailure(error: error)),
+      (success) async {
+        final refreshResult = await kendaraanRepository.getAllKendaraan();
+        refreshResult.fold(
+          (error) => emit(KendaraanFailure(error: error)),
+          (data) => emit(KendaraanLoaded(listKendaraan: data.dataKendaraan)),
+        );
+      },
+    );
+  }
+}
