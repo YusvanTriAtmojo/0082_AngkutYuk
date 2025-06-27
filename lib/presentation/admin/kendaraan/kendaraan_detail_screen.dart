810ace623815252f217af014640812ef6f6f944d
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-06-27 16:51:48 +0700

    Menambahkan halaman detail kendaraan

---
+++ lib/presentation/admin/kendaraan/kendaraan_detail_screen.dart
@@ -0,0 +1,125 @@
+import 'package:angkut_yuk/data/model/response/get_all_kendaraan_response_model.dart';
+import 'package:angkut_yuk/presentation/admin/bloc/kendaraan/kendaraan_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:angkut_yuk/core/color/color.dart';
+
+class KendaraanDetailScreen extends StatelessWidget {
+  final Kendaraan kendaraan;
+
+  const KendaraanDetailScreen({
+    super.key, 
+    required this.kendaraan});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Detail Kendaraan"),
+        backgroundColor: Warna.ungu,
+        foregroundColor: Colors.white,
+        centerTitle: true,
+      ),
+      backgroundColor: Warna.ungubackgorund,
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Warna.ungu,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Container(
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Warna.ungubackgorund,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.only(left: 60, right: 30),
+                  child: Column(
+                    children: [
+                      dataRow("Nama", kendaraan.namaKendaraan),
+                      SizedBox(height: 10),
+                      dataRow("Plat Nomor", kendaraan.platNomor),
+                      SizedBox(height: 10),
+                      dataRow("Muatan", "${kendaraan.kapasitasMuatan} kg"),
+                      SizedBox(height: 10),
+                      dataRow("Status", kendaraan.statusKendaraan),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 30,),
+              ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (dialogContext) {
+                      return AlertDialog(
+                        title: Text("Konfirmasi"),
+                        content: Text("Yakin ingin menghapus kendaraan ini?"),
+                        actions: [
+                          TextButton(
+                            child: Text("Batal"),
+                            onPressed: () => Navigator.of(dialogContext).pop(),
+                          ),
+                          TextButton(
+                            child: Text("Hapus", style: TextStyle(color: Colors.red)),
+                            onPressed: () {
+                              Navigator.of(dialogContext).pop();
+                              context.read<KendaraanBloc>().add(KendaraanDeleted(kendaraan.idKendaraan),);
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  foregroundColor: Colors.white,
+                  minimumSize: Size(150, 48), 
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                ),
+                child: Text(
+                  "Hapus",
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget dataRow(String label, String value) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          width: 100,
+          child: Text(
+            "$label:",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+        ),
+        Expanded(
+          child: Text(
+            value,
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+      ],
+    );
+  }
+}
