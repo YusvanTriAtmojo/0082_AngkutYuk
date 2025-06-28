299343d0e15756a8d0e8046da320ab618306101d
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-06-28 19:02:59 +0700

    Melengkapi bloc dengan petugas bloc yang mengelola get, create, delete

---
+++ lib/presentation/admin/bloc/petugas/petugas_bloc.dart
@@ -0,0 +1,68 @@
+import 'package:angkut_yuk/data/model/request/admin/petugas_request_model.dart';
+import 'package:angkut_yuk/data/model/response/get_all_petugas_response_model.dart';
+import 'package:angkut_yuk/data/repository/petugas_repository.dart';
+import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:meta/meta.dart';
+
+part 'petugas_event.dart';
+part 'petugas_state.dart';
+
+class PetugasBloc extends Bloc<PetugasEvent, PetugasState> {
+  final PetugasRepository petugasRepository;
+
+  PetugasBloc({required this.petugasRepository}) : super(PetugasInitial()) {
+    on<PetugasRequested>(_onPetugasRequested);
+    on<PetugasCreateRequested>(_onPetugasCreateRequested);
+    on<PetugasDeleted>(_onPetugasDeleted);
+  }
+
+  Future<void> _onPetugasRequested(
+    PetugasRequested event,
+    Emitter<PetugasState> emit,
+  ) async {
+    emit(PetugasLoading());
+
+    final Either<String, GetAllPetugasModel> result =
+        await petugasRepository.getAllPetugas();
+
+    result.fold(
+      (failure) => emit(PetugasFailure(error: failure)),
+      (data) => emit(PetugasLoaded(listPetugas: data.dataPetugas)),
+    );
+  }
+
+  Future<void> _onPetugasCreateRequested(
+    PetugasCreateRequested event,
+    Emitter<PetugasState> emit,
+  ) async {
+    emit(PetugasLoading());
+
+    final result = await petugasRepository.createPetugas(event.requestModel);
+
+    result.fold(
+      (failure) => emit(PetugasFailure(error: failure)),
+      (message) => emit(PetugasOperationSuccess(message: message)),
+    );
+  }
+
+  Future<void> _onPetugasDeleted(
+    PetugasDeleted event,
+    Emitter<PetugasState> emit,
+  ) async {
+    emit(PetugasLoading());
+
+    final result = await petugasRepository.deletePetugas(event.id);
+
+    await result.fold(
+      (error) async => emit(PetugasFailure(error: error)),
+      (success) async {
+        final refreshResult = await petugasRepository.getAllPetugas();
+        refreshResult.fold(
+          (error) => emit(PetugasFailure(error: error)),
+          (data) => emit(PetugasLoaded(listPetugas: data.dataPetugas)),
+        );
+      },
+    );
+  }
+}
