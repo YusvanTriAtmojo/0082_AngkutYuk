69a011fa5f4073e3923bba35e7bc59d69844507c
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-06-28 18:52:42 +0700

    Menambahkan petugas state untuk kondisi tampilan

---
+++ lib/presentation/admin/bloc/petugas/petugas_state.dart
@@ -0,0 +1,25 @@
+part of 'petugas_bloc.dart';
+
+sealed class PetugasState {}
+
+final class PetugasInitial extends PetugasState {}
+
+final class PetugasLoading extends PetugasState {}
+
+final class PetugasLoaded extends PetugasState {
+  final List<Petugas> listPetugas;
+
+  PetugasLoaded({required this.listPetugas});
+}
+
+final class PetugasOperationSuccess extends PetugasState {
+  final String message;
+
+  PetugasOperationSuccess({required this.message});
+}
+
+final class PetugasFailure extends PetugasState {
+  final String error;
+
+  PetugasFailure({required this.error});
+}
