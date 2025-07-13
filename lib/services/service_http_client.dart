fab8d633123aff85abb044f19e8b2ead58bd16a5
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2025-07-14 05:58:34 +0700

    Menambahkan service uploadFile

--- lib/services/service_http_client.dart
+++ lib/services/service_http_client.dart
@@ -2,9 +2,11 @@ import 'dart:convert';
 
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
+import 'package:path/path.dart';
+import 'dart:io';
 
 class ServiceHttpClient {
-  final String baseUrl = 'http://10.0.2.2:8000/api/';
+  final String baseUrl = 'http://192.168.100.6:8000/api/';
   final secureStorage = FlutterSecureStorage();
 
   Future<http.Response> post(String endPoint, Map<String, dynamic> body) async {
@@ -102,5 +104,24 @@ class ServiceHttpClient {
     } catch (e) {
       throw Exception("DELETE request with token failed: $e");
     }
+  }
+
+  Future<http.StreamedResponse> uploadFile({
+    required String endPoint,
+    required File file,
+    required String fieldName,
+  }) async {
+    final token = await secureStorage.read(key: "authToken");
+    final url = Uri.parse("$baseUrl$endPoint");
+
+    final request = http.MultipartRequest('POST', url)
+      ..headers['Authorization'] = 'Bearer $token'
+      ..files.add(await http.MultipartFile.fromPath(
+        fieldName,
+        file.path,
+        filename: basename(file.path),
+      ));
+
+    return await request.send();
   }
 }
